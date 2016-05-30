242f4f4255707962d2673d54616ac38748f5d2af
darren <764190362@qq.com>
2016-05-30 13:51:20 +0800

    完成基本概念

--- app/index.js
+++ app/index.js
@@ -26,58 +26,92 @@
 //);
 
 
-
+//组件嵌套
 //组件的几个要求：focused, independent, reusable, small, testable
 
-var USER_DATA = {
-    name: 'darren',
-    username: 'DarrenActive',
-    image: 'https://avatars0.githubusercontent.com/u/2933430?v=38s=460'
-};
+//var USER_DATA = {
+//    name: 'darren',
+//    username: 'DarrenActive',
+//    image: 'https://avatars0.githubusercontent.com/u/2933430?v=38s=460'
+//};
+//
+//
+//var React = require('react');
+//var ReactDOM = require('react-dom');
+//
+////被嵌套组件
+//var ProfilePic = React.createClass({
+//    render: function(){
+//        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+//    }
+//})
+//
+//var Link = React.createClass({
+//    
+//    changeURL: function(){
+//        window.location.replace(this.props.href);
+//    },
+//    
+//    render: function(){
+//        return (
+//            <span 
+//                style={{color: 'blue', cursor: 'pointer'}}
+//                onClick={this.changeURL}>
+//                {this.props.children}
+//            </span>
+//        )
+//    }
+//});
+//
+////被嵌套组件
+//var ProfileLink = React.createClass({
+//    render: function(){
+//        return (
+//            <div>
+//                <Link href={'https://www.github.com/' + this.props.username}>
+//                    {this.props.username}
+//                </Link>
+//            </div>
+//        )
+//    }
+//});
+//
+////被嵌套组件
+//var ProfileName = React.createClass({
+//    render: function(){
+//        return (
+//            <div>{this.props.name}</div>
+//        )
+//    }
+//})
+//
+////嵌套组件
+//var Avatar = React.createClass({
+//    render: function(){
+//        return (
+//            <div>
+//                <ProfilePic imageUrl={this.props.user.image} />
+//                <ProfileName name={this.props.user.name} />
+//                <ProfileLink username={this.props.user.username} />
+//            </div>
+//        )
+//    }
+//})
+//
+//ReactDOM.render(
+//    <Avatar user={USER_DATA} />,
+//    document.getElementById('app')
+//);
 
 
+
+//路由
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-    render: function(){
-        return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-    }
-})
-
-var ProfileLink = React.createClass({
-    render: function(){
-        return (
-            <div>
-                <a href={'https://www.github.com/' + this.props.username}>
-                    {this.props.username}
-                </a>
-            </div>
-        )
-    }
-});
-
-var ProfileName = React.createClass({
-    render: function(){
-        return (
-            <div>{this.props.name}</div>
-        )
-    }
-})
-
-var Avatar = React.createClass({
-    render: function(){
-        return (
-            <div>
-                <ProfilePic imageUrl={this.props.user.image} />
-                <ProfileName name={this.props.user.name} />
-                <ProfileLink username={this.props.user.username} />
-            </div>
-        )
-    }
-})
+var routes = require('./config/routes');
 
 ReactDOM.render(
-    <Avatar user={USER_DATA} />,
+    routes,
     document.getElementById('app')
 );
+
